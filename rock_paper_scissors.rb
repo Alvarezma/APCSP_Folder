8a0e9ef8470ba94e46c841795bf7bd10b4a4462f
Alvarezma <Derpachu@comcast.net>
2018-10-02 10:22:54 -0600

    Part 3

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -92,3 +92,86 @@
 # end
 
 # puts "TRY AGAIN ;)"
+
+# [Part 3]
+# [Try 1 strategy]
+player_score = 0
+computer_score = 0
+wind = false
+
+while (player_score < 4 && computer_score < 4)
+    puts "\nEnter rock, paper, or scissors"
+    $stdout.flush
+    player = gets.chomp
+    if (player_score == 0 && computer_score == 0)
+        computer = ["rock", "paper", "scissors"].sample
+    end
+    puts "\nComputer plays: #{computer}\n"
+    if ((player == "paper" && computer == "rock") || (player == "rock" && computer == "scissors") || (player == "scissors" && computer == "paper"))
+        puts "you get a point"
+        player_score += 1
+        win = false
+    elsif (player == computer)
+        puts "Tie"
+    else
+        puts "Computer gets a point"
+        computer_score += 1
+        win = true
+    end
+    if win
+        computer = player
+    else
+        if (computer == "scissors")
+            computer = "paper"
+        end
+        if (computer == "rock")
+            computer = "scissors"
+        end
+        if (computer == "paper")
+            computer = "rock"
+        end
+    end
+    puts "your score: #{player_score}"
+    puts "computer score: #{computer_score}"
+end
+
+if (player_score == 4) 
+    puts "you win!!!"
+else
+    puts "TRY AGAIN ;)"
+end
+
+# [Try 2 probability]
+
+player_score = 0
+computer_score = 0
+rock = 0
+paper = 0
+scissors = 0
+
+while (player_score < 4 && computer_score < 4)
+    puts "\nEnter rock, paper, or scissors"
+    $stdout.flush
+    player = gets.chomp
+    if (player_score == 0 && computer_score == 0)
+        computer = ["rock", "paper", "scissors"].sample
+    end
+    puts "\nComputer plays: #{computer}\n"
+    if ((player == "paper" && computer == "rock") || (player == "rock" && computer == "scissors") || (player == "scissors" && computer == "paper"))
+        puts "you get a point"
+        player_score += 1
+    elsif (player == computer)
+        puts "Tie"
+    else
+        puts "Computer gets a point"
+        computer_score += 1
+    end
+    puts "your score: #{player_score}"
+    puts "computer score: #{computer_score}"
+end
+
+if (player_score == 4) 
+    puts "you win!!!"
+else
+    puts "TRY AGAIN ;)"
+end
