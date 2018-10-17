e093d4e4b3b0d406573bb524db0f2f5f946c05e3
Alvarezma <Derpachu@comcast.net>
2018-10-17 11:02:32 -0600

    edits

--- heights/heights.rb
+++ heights/heights.rb
@@ -17,8 +17,8 @@ def find_average(data)
     num_of_heights = 0
 
     data.each do |n|
-        sum = sum + n.to_i
-        num_of_heights = num_of_heights + 1
+        sum += n.to_i
+        num_of_heights += 1
     end
 
     return (sum + 0.0) / num_of_heights
@@ -28,13 +28,35 @@ puts find_average(data)
 
 def find_standard_deviation(data)
     average = find_average(data)
-    sqrd_diff = Array.new
+    sum = 0
+    data_length = 0
     
     data.each do |n|
-        sqrd_diff += [(n.to_i - average) * (n.to_i - average)]
+        sum += (n.to_i - average) * (n.to_i - average)
+        data_length += 1
     end
     
-    return Math.sqrt(find_average(sqrd_diff))
+    return Math.sqrt(sum / data_length)
 end
 
 puts find_standard_deviation(data)
+
+def find_mode(data)
+    best_match = 0
+    mode = ""
+    data.each do |x|
+        match = 0
+        data.each do |y|
+            if x == y
+                match += 1
+            end
+        end
+        if match > best_match
+            mode = x
+            best_match = match
+        end
+    end
+    return mode
+end
+
+puts find_mode(data)
