27f13f4eb7df4cd1b67ed777da313c55d7f83346
Alvarezma <Derpachu@comcast.net>
2018-10-22 10:24:18 -0600

    Colors

---
+++ popular_color/colors.rb
@@ -0,0 +1,16 @@
+file = open("./colors.csv")
+
+lines = file.readline
+
+puts lines
+
+
+# names = []
+# fav = []
+# sec_fav = []
+# lines.each do |n|
+#     data = lines[n].split(',')
+#     names.push(data[0])
+#     fav.push(data[1])
+#     sec_fav.push(data[2])
+# end
