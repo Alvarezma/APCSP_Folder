d696275334fd4981b9c799756dfe26baf6f85e90
Alvarezma <Derpachu@comcast.net>
2018-11-14 10:50:47 -0700

    Minor fixes

--- dog_quiz/dogs.rb
+++ dog_quiz/dogs.rb
@@ -4,13 +4,13 @@ lines = open("./dogs.csv").read.split(/\n/)
 names = []
 breeds = []
 ages = []
-happiness = []
+happiness_ratings = []
 lines.each do |n|
     data = n.split(',')
     names.push(data[0])
     breeds.push(data[1])
-    ages.push(data[2])
-    happiness.push(data[3])
+    ages.push(data[2].to_i)
+    happiness_ratings.push(data[3].to_f)
 end
 
 # Problem 0
@@ -33,7 +33,7 @@ puts ""
 def ringworm_shots(names, ages)
     list_of_9yo_dogs = []
     names.length.times do |i|
-        if ages[i] == "9"
+        if ages[i] == 9
             list_of_9yo_dogs.push(names[i])
         end
     end
@@ -45,33 +45,33 @@ puts ""
 
 # Problem 2
 
-def happiest_dog_of_breed(target_breed, names, breeds, happiness)
+def happiest_dog_of_breed(target_breed, names, breeds, happiness_ratings)
     greatest_happiness = 0.0
     happiest_dog = ""
     breeds.length.times do |i|
-        if breeds[i] == target_breed && happiness[i].to_f > greatest_happiness
+        if breeds[i] == target_breed && happiness_ratings[i] > greatest_happiness
             happiest_dog = names[i]
 
-            greatest_happiness = happiness[i].to_f
+            greatest_happiness = happiness_ratings[i]
         end
     end
     return happiest_dog
 end
 
-puts happiest_dog_of_breed("Pointer", names, breeds, happiness)
-puts happiest_dog_of_breed("Corgi", names, breeds, happiness)
+puts happiest_dog_of_breed("Pointer", names, breeds, happiness_ratings)
+puts happiest_dog_of_breed("Corgi", names, breeds, happiness_ratings)
 puts ""
 
 # Problem 3
 
-def happiest_breed(breeds, happiness)
+def happiest_breed(breeds, happiness_ratings)
     greatest_happiness_avg = 0
     happiest_breed = ""
-    breeds.each do |n|
-        avg = avg_happiness_of_breed(n, breeds, happiness)
+    breeds.each do |name_of_breed|
+        avg = avg_happiness_of_breed(name_of_breed, breeds, happiness_ratings)
 
         if avg > greatest_happiness_avg
-            happiest_breed = n
+            happiest_breed = name_of_breed
 
             greatest_happiness_avg = avg
         end
@@ -79,17 +79,17 @@ def happiest_breed(breeds, happiness)
     return happiest_breed
 end
 
-def avg_happiness_of_breed(target_breed, breeds, happiness)
+def avg_happiness_of_breed(target_breed, breeds, happiness_ratings)
     sum = 0.0
     dogs_with_target_breed = 0
     breeds.length.times do |i|
         if breeds[i] == target_breed
-            sum += happiness[i].to_f
+            sum += happiness_ratings[i]
             dogs_with_target_breed += 1
         end
     end
     return sum / dogs_with_target_breed
 end
 
-puts happiest_breed(breeds, happiness)
+puts happiest_breed(breeds, happiness_ratings)
 puts ""
